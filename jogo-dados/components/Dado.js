acce48b3933015538ddc86abed0d2afa79b60b60
AndreMadrug <felipemadruga2011@hotmail.com>
2026-09-13 19:56:40 -0300

    alterações
    Removed component documentation comments from Dado.js.

--- jogo-dados/components/Dado.js
+++ jogo-dados/components/Dado.js
@@ -1,10 +1,5 @@
 import styles from "./Dado.module.css";
 
-/**
- * Componente Dado
- * Recebe uma prop `valor` (número de 1 a 6, ou null/undefined
- * quando o dado ainda não foi jogado) e mostra a imagem correspondente.
- */
 export default function Dado({ valor }) {
   const src = valor ? `/dados/${valor}.svg` : "/dados/vazio.svg";
   const alt = valor ? `Dado mostrando o número ${valor}` : "Dado ainda não jogado";
